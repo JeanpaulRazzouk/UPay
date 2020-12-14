24cc7e0b7bf966315959b54b11d0f779caabc6bf
JohnPaul Razzouk <johnpaulrazzouk@gmail.com>
2020-12-14 12:30:59 +0200

    slight adjust

--- UPay/app/src/main/java/com/example/Profile/PersonalInfoFragment.java
+++ UPay/app/src/main/java/com/example/Profile/PersonalInfoFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,6 +21,8 @@ import com.example.Adapters.Adapter2;
 import com.example.upay.BottomSheetPayPal;
 import com.example.upay.PersInfoData;
 import com.example.upay.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -41,6 +45,8 @@ public class PersonalInfoFragment  extends BottomSheetDialogFragment {
         PersArray = new ArrayList<>();
         adapter = new Adapter2(getContext(), PersArray);
         recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
+                DividerItemDecoration.VERTICAL));
         //
         Activity();
         return  view;
@@ -50,12 +56,14 @@ public class PersonalInfoFragment  extends BottomSheetDialogFragment {
     public void Activity (){
         user = FirebaseAuth.getInstance().getCurrentUser();
         //
-        String t [] = new String[3];
+        String t [] = new String[4];
         t[0] = user.getEmail();
         t[1] = "Password (N/S)";
         t[2] = "N/A"+user.getPhoneNumber();
+        t[3] = "N/A";
+
         //
-        for (int i = 0; i<=2 ;i++) {
+        for (int i = 0; i<=3 ;i++) {
             PersInfoData p = new PersInfoData(t[i]);
             PersArray.add(p);
         }
--- UPay/app/src/main/java/com/example/RecycleHolders/RecyclerHolder2.java
+++ UPay/app/src/main/java/com/example/RecycleHolders/RecyclerHolder2.java
@@ -14,16 +14,9 @@ import com.example.upay.R;
 // Recycler Holder 2 is For Personal Info Fragment;
 public class RecyclerHolder2 extends RecyclerView.ViewHolder {
     private TextView Data;
-    private CardView cardView;
-    Animation animation;
     public RecyclerHolder2(View itemView) {
         super(itemView);
         Data = itemView.findViewById(R.id.txtOutput);
-        //
-        cardView = itemView.findViewById(R.id.income);
-        animation = AnimationUtils.loadAnimation(itemView.getContext(), R.anim.open_animation);
-        animation.setDuration(1200);
-        cardView.setAnimation(animation);
     }
 
     public void setDetails(PersInfoData items) {
